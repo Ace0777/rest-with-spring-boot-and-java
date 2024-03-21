62b820e57ed66af788afb3831ccc8f93aaa6966c
Ace0777 <luishhasantos@gmail.com>
2024-03-21 06:03:32 -0300

    Ajustando req http no controller de paciente

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -23,16 +23,14 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder){
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder){
 
         var medico = new Medico(dados);
         repository.save(medico);
 
-
         var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
 
-
-        return ResponseEntity.created(uri).body();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
 
     }
     @GetMapping
@@ -55,8 +53,4 @@ public class MedicoController {
 
         return ResponseEntity.noContent().build();
     }
-
-
-
-
 }
