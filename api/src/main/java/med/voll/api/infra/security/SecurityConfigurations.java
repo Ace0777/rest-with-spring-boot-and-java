a053269cf6fb3787eec827fa309041f043041e93
Ace0777 <luishhasantos@gmail.com>
2024-03-27 18:35:25 -0300

    Finalizando configuração do spring security e realizando fix nas validações de tokens

--- api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,8 +1,10 @@
 package med.voll.api.infra.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,19 +13,29 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable()) //desabilitando tratamento contra ateque CSRF
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        return http.csrf().disable() //desabilitando tratamento contra ateque CSRF
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 // desabilitando processo de autenticação com formulario e passando auth pra stateless
+                .and().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
+
+
 
 
     //metodo que ensina o spring a injetar objetos
